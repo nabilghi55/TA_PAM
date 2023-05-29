7b0e1b012c6f7a3fbe9abc66f6b961c5eb89ba79
nabilghi55 <89720241+nabilghi55@users.noreply.github.com>
2023-05-29 20:58:45 +0700

    daftar

--- app/src/main/java/com/example/ta_pam/AyoMulai.java
+++ app/src/main/java/com/example/ta_pam/AyoMulai.java
@@ -2,7 +2,10 @@ package com.example.ta_pam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AyoMulai extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class AyoMulai extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayo_mulai);
+
+        Button btn_Daftar;
+        Button btn_Login;
+
+        btn_Login  = findViewById(R.id.login);
+        btn_Daftar = findViewById(R.id.daftar);
+
+        btn_Daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AyoMulai.this,Daftar.class);
+                startActivity(intent);
+            }
+        });
     }
 }
