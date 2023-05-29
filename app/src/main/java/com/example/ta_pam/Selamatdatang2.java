caf3bd786a13988088db2b0be392a8d1e6eb2624
Moch Danial Rasyid Akbar S <danialrasyid1@student.ub.ac.id>
2023-05-29 21:07:23 +0700

    selamatdatang2

---
+++ app/src/main/java/com/example/ta_pam/Selamatdatang2.java
@@ -0,0 +1,13 @@
+package com.example.ta_pam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Selamatdatang2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selamat_datang2);
+    }
+}
