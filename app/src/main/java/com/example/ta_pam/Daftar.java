5f2d756e295ba6012da5e11573da3bb4713ab9aa
nabilghi55 <89720241+nabilghi55@users.noreply.github.com>
2023-05-29 23:04:17 +0700

    ha

--- app/src/main/java/com/example/ta_pam/Daftar.java
+++ app/src/main/java/com/example/ta_pam/Daftar.java
@@ -1,14 +1,59 @@
 package com.example.ta_pam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Daftar extends AppCompatActivity {
-
+    EditText inputUsername, inputEmail, inputPassword, inputNoTel;
+    String username, email, password, noTel;
+    Button btn_daftar;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
+        inputUsername = findViewById(R.id.edit_username);
+        inputEmail = findViewById(R.id.edit_email);
+        inputPassword = findViewById(R.id.edit_password);
+        inputNoTel = findViewById(R.id.edit_telepon);
+        btn_daftar = findViewById(R.id.btn_daftar);
+        btn_daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registrasi();
+            }
+        });
+
+    }
+
+    private void registrasi() {
+        email = inputEmail.getText().toString();
+        username = inputUsername.getText().toString();
+        noTel = inputNoTel.getText().toString();
+        password = inputPassword.getText().toString();
+        mAuth.createUserWithEmailAndPassword(username,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Daftar.this,"Registrasi Berhasil", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(Daftar.this,"Registrasi Gagal", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
     }
 }
